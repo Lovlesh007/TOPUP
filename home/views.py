8fb62ae5cb2dc6d3123597abbacb5851955a69bf
Lovlesh Bhatt <Lovleshbhatt@users.noreply.github.com>
2019-10-13 04:09:28 +0530

    Project is Completed according to me
    completed From my side

--- home/views.py
+++ home/views.py
@@ -29,16 +29,39 @@ def homeid(request,home_id):
 def search(request):
 	if request.method =='GET':
 		search_value =request.GET['search']
-		search_result =Mobile_recharge.objects.filter(
+		search_result_for_mobile =Mobile_recharge.objects.filter(
 			Q(Operator__icontains=search_value) |
 			Q(Recharge_Type__icontains=search_value) |
 			Q(Circle__icontains=search_value) |
 			Q(Recharge_Amount__iexact=search_value) |
 			Q(Enter_Your_Mobile_Number__iexact=search_value)
+			)
+
+		search_result_for_dth =Dth_recharge.objects.filter(
+			Q(Select_DTH_Operator__icontains= search_value) |
+			Q(Customer_id__iexact=search_value) |
+			Q(Recharge_Amount__iexact=search_value)
+			)
+
+		search_result_for_datacard =Datacard_recharge.objects.filter(
+			Q(Data_Card_Number__iexact= search_value) |
+			Q(Select_Datacard_Operator__iexact=search_value) |
+			Q(Recharge_Amount__iexact=search_value)
+			)
 
 
-		)  # In Our Mobile_recharge Model it will get all the objects whose Operator actually contains the search_term
-		return render(request,'home/search.html',{'show':search_value,'result':search_result.filter(Currentuser=request.user)})
+		search_result_for_metro=Metro_recharge.objects.filter(
+			Q(Select_Metro_Type__icontains= search_value) |
+			Q(Card_Number__iexact=search_value) |
+			Q(Recharge_Amount__iexact=search_value)
+			)
+
+
+
+
+
+		  # In Our Mobile_recharge Model it will get all the objects whose Operator actually contains the search_term
+		return render(request,'home/search.html',{'show':search_value,'mobileresult':search_result_for_mobile.filter(Currentuser=request.user),'dthrecharge':search_result_for_dth.filter(Currentuser=request.user),'datacardrecharge':search_result_for_datacard.filter(Currentuser=request.user),'metrorecharge':search_result_for_metro.filter(Currentuser=request.user)})
 	else:
 		return render(request,'home/index.html')
 
@@ -64,3 +87,4 @@ def history(request):
 
 	#HERE WE CANNOT USE THE icontains feature here because icontains only used when the input coming is dynamic or unpredictable or depend on user but here we know the matching term is fixed i.e., request.user so it is static so we only use assign operator
 	return render(request,'home/history.html',{'mobile':result_of_mobile,'dth':result_of_dth,'datacard':result_of_datacard,'metro':result_of_metro})
+
